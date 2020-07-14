ec91b3c5135eba2ebf10491b1328d92d4146b13a
Vismark Juarez <vismarkjuarez@gmail.com>
2020-07-14 13:41:00 -0500

    Fixed port number mapping issues

--- node.js
+++ node.js
@@ -19,9 +19,6 @@ const multer = require('multer')
 //specifies that this app will be using express.
 const app = express();
 
-//multer middle-ware. Needed for downloaded files from HTML forms
-var upload = multer({ storage: storage });
-
 //middleware for processing POST requests a bit easier.
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -32,7 +29,7 @@ app.use(express.static(__dirname + "/resources"));
 
 
 //Start-up behaviour.
-var port = process.env.PORT || config.server.port; //process.env.port will only be used if running on heroku
+var port = process.env.PORT || 2020; //process.env.port will only be used if running on heroku
 app.listen(port, function() {
     console.log("You may access the server locally via: http://localhost:" + port);
   });
